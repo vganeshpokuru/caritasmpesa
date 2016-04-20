0e9a9ff50d8e25cb27f9e98ffff724e58635b03b
awaleram <ram.awale@confuxtechnologies.com>
2016-04-20 09:58:41 +0530

    mpesa improvement on top of phase2

---
+++ src/main/java/org/mifosplatform/mpesa/domain/MpesaBranchMapping.java
@@ -0,0 +1,43 @@
+package org.mifosplatform.mpesa.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity 
+@Table(name="mpesatxnbranchmapping")
+public class MpesaBranchMapping {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name="office_id")
+	private Long office_id;
+	
+	@Column(name="MpesaPayBillNumber")
+	private String MpesaPayBillNumber;
+
+	public Long getOffice_id() {
+		return office_id;
+	}
+
+	public void setOffice_id(Long office_id) {
+		this.office_id = office_id;
+	}
+
+	public String getMpesaPayBillNumber() {
+		return MpesaPayBillNumber;
+	}
+
+	public void setMpesaPayBillNumber(String MpesaPayBillNumber) {
+		this.MpesaPayBillNumber = MpesaPayBillNumber;
+	}
+	
+	
+	
+}
