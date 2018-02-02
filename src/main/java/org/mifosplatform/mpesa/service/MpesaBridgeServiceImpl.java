ead86fdfc98cfaa2a010a8cc36693c427269696f
venkat <venkata.ganesh@confluxtechnologies.com>
2018-02-02 14:38:47 +0530

    change response

--- src/main/java/org/mifosplatform/mpesa/service/MpesaBridgeServiceImpl.java
+++ src/main/java/org/mifosplatform/mpesa/service/MpesaBridgeServiceImpl.java
@@ -161,7 +161,7 @@ public class MpesaBridgeServiceImpl implements MpesaBridgeService {
 				}
 				response = this.mpesaBridgeRepository.save(mpesa);
 				if (response != null) {
-					responseData = "OK|Thank you for your payment";
+					responseData = "OK|Thank you";
 				}
 			} else {
 				logger.info("Empty Parameter passed");
